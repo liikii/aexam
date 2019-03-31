9e5ce329d45d5be4dab7a6633f7d1f91aadae7cd
liikii <609957891@qq.com>
2019-03-31 21:51:24 +0800

    u

---
+++ hello2.asm
@@ -0,0 +1,18 @@
+segment .text
+    global _start
+
+
+_start:
+    mov edx, len
+    mov ecx, msg
+    mov ebx, 1
+    mov eax, 4
+    int 0x80
+
+    mov eax,1
+    int 0x80
+
+
+segment .data
+msg db 'hello world'
+len equ $-msg
